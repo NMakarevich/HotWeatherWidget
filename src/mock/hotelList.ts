aa51a404ec8db13448c839f3db2f6109d68a7363
Nikolay Makarevich <nikoladze529@mail.ru>
2021-04-11 19:15:34 +0300

    fix

--- src/mock/hotelList.ts
+++ src/mock/hotelList.ts
@@ -19,7 +19,7 @@ export interface IHotelList {
 
 export const hotelList: IHotelList[] = [
   {
-    img: '/assets/img/Turkey/Alanya/white_gold/1.jpg',
+    img: 'assets/img/Turkey/Alanya/white_gold/1.jpg',
     adress: 'Gol Mah, Dadaşlar Cd. 2/1, 07460',
     phone: '+90 242 255 05 41',
     weather: {
@@ -30,14 +30,14 @@ export const hotelList: IHotelList[] = [
     },
     social_info: {
       title: 'WHITE GOLD HOTEL & SPA',
-      img: '/assets/img/Turkey/Alanya/white_gold/2.jpg',
+      img: 'assets/img/Turkey/Alanya/white_gold/2.jpg',
       folowers: 2850,
       following: 675
     },
     type: 'Turkey'
   },
   {
-    img: '/assets/img/Turkey/Antalya/sherwood_exclusive/1.jpg',
+    img: 'assets/img/Turkey/Antalya/sherwood_exclusive/1.jpg',
     adress: 'Kemerağzı Kopak Çay Mevkii Kemerağzı Mahalles, Lara Cd., 07112 Aksu',
     phone: '+90 242 352 28 00',
     weather: {
@@ -48,14 +48,14 @@ export const hotelList: IHotelList[] = [
     },
     social_info: {
       title: 'SHERWOOD EXCLUSIVE LARA',
-      img: '/assets/img/Turkey/Antalya/sherwood_exclusive/2.jpg',
+      img: 'assets/img/Turkey/Antalya/sherwood_exclusive/2.jpg',
       folowers: 3000,
       following: 500
     },
     type: 'Turkey'
   },
   {
-    img: '/assets/img/Greece/Rodos/semiramis/1.jpg',
+    img: 'assets/img/Greece/Rodos/semiramis/1.jpg',
     adress: 'I. Metaxa 18, Rodos 851 31',
     phone: '+30 2241 020741',
     weather: {
@@ -66,7 +66,7 @@ export const hotelList: IHotelList[] = [
     },
     social_info: {
       title: 'SEMIRAMIS CITY HOTEL',
-      img: '/assets/img/Greece/Rodos/semiramis/2.jpg',
+      img: 'assets/img/Greece/Rodos/semiramis/2.jpg',
       folowers: 3454,
       following: 488
     },
